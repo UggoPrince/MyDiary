35432b709a95489fe0a23d6158cf85eb01a91700
uggoprince <uggoprince@gmail.com>
2018-07-22 00:16:36 +0100

    update in index.js from app.listen(4000) to app.listen(port, ()=>{console.log(message + port);})

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./routes/users";
 const app = express();
+let port = process.env.PORT || 4000;
 
 app.use("/api/v1/users", router);
 
@@ -9,6 +10,8 @@ app.use((req, res)=>{
     res.status(404).json("Not Found");
 });
 
-app.listen(4000);
+app.listen(port, ()=>{
+    console.log("App running on port " + port);
+});
 //app.set("port", process.env.PORT || 3000);
 //export default app;
